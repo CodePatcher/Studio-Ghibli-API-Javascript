3996d1a4475790d9d234bef72b03503bdc2ccfab
CodePatcher <mikael.hailemichael@gmail.com>
2018-04-14 11:16:24 +0200

    Added javascript to the project

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
+
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      movie["isFavorite"] = false;
+      console.log(movie);
+
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      card.setAttribute('id', `${movie.title}`);
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+
+    });
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
